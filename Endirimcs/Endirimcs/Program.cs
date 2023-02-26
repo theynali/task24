51bc1593653a23e6c0d43d006881540d3ea6b6ff
theynali <izeynalli76@gmail.com>
2023-02-26 17:57:21 +0400

    salam

---
+++ Endirimcs/Endirimcs/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace endirimcs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        { Console.WriteLine("Mebleq: ");
+
+            int mebleq = Convert.ToInt32(Console.ReadLine());
+            if (mebleq < 100)
+            {
+                Console.WriteLine(mebleq - (mebleq * 5 / 100));
+            }
+            else if (mebleq > 100 && mebleq < 1000) 
+            {
+                Console.WriteLine(mebleq - (mebleq * 8 / 100));
+            }
+            else
+            {
+
+                Console.WriteLine(mebleq - mebleq * 10 / 1000);
+            }
+
+            
+        }
+    }
+}
